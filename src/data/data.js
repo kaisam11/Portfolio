b20af0bbe73d78972a65c74870b41337cebd12a0
Hrithik <hrithiksinha17@gmail.com>
2022-07-28 01:32:47 +0530

    project update

--- src/data/data.js
+++ src/data/data.js
@@ -8,6 +8,8 @@ import efa from '../images/EFA.webp'
 import efa2 from '../images/EFA2.webp'
 import caffe from '../images/Caffe.webp'
 import caffe2 from '../images/Caffe2.webp'
+import pawdopt from '../images/Pawdopt.webp'
+import pawdopt2 from '../images/Pawdopt2.webp'
 import one from '../images/1.jpg'
 import two from '../images/2.jpg'
 import three from '../images/3.jpeg'
@@ -16,37 +18,46 @@ import four from '../images/4.jpg'
 export const projects = [
     {
         id: 1,
+        img: pawdopt,
+        title: 'Pawdopt App',
+        img2: pawdopt2,
+        distort: one,
+        type: 'UX UI Design',
+        link: 'https://www.behance.net/gallery/149122539/Pawdopt-UX-Case-Study'
+    },
+    {
+        id: 2,
         img: amazon,
         title: 'Amazon App Redesign',
         img2: amazon2,
-        distort: one,
+        distort: two,
         type: 'UX UI Design',
         link: 'https://www.behance.net/gallery/146011057/Amazon-App-Redesign'
     },
     {
-        id: 2,
+        id: 3,
         img: foodDeliveryApp,
         title: 'Food Delivery',
         img2: foodDeliveryApp2,
-        distort: two,
+        distort: three,
         type: 'UX UI Design',
         link: 'https://www.behance.net/gallery/134438563/Food-Delivery-App-Design'
     },
     {
-        id: 3,
+        id: 4,
         img: divertical,
         title: 'Divertical Website',
         img2: divertical2,
-        distort: three,
+        distort: four,
         type: 'UX UI Design',
         link: 'https://www.behance.net/gallery/144556863/UX-UI-Design-Divertical'
     },
     {
-        id: 4,
+        id: 5,
         img: efa,
         title: 'Football Association',
         img2: efa2,
-        distort: four,
+        distort: one,
         type: 'Web Design',
         link: 'https://www.behance.net/gallery/120495377/European-Football-Association-Webdesign-Concept'
     }
